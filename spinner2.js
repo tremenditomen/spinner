7d7fff3a980519009fb1a60b53752acb3928698a
Luis Andres Contreras <tremenditomen@gmail.com>
2021-10-03 19:21:48 -0600

    refactored spinner

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+const arrayFrames = ['|', '/','-','\\','|', '/','-','\\','|'];
+  let counter = 100;
+
+  for(const item of arrayFrames) {
+    setTimeout(() => {
+      process.stdout.write(`\r${item}`)
+    }, counter)
+    counter += 200;
+  }
+  
+  
+
+
+
